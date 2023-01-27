b346271a67435d3ffc9528dfd0d6eba2cf37ac6f
Patricio Torres <patriciotorres1992@hotmail.com>
2023-01-26 22:50:01 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -2,17 +2,6 @@ const nombre = prompt("ingrese su Nombre");
 console.log(nombre);
 let edad = prompt("ingrese su edad");
 let PrecioTotal = 0;
-// if (edad < 18) {
-//   alert("tienes que ser mayor de 18 años para comprar");
-//   edad = parseInt(prompt("ingrese su edad"));
-// } else {
-//   const articulos = parseInt(prompt("ingrese numero de articulos"));
-//   for (let i = 0; (i = articulos); i++) {
-//     let precio = parseInt(prompt("ingrese precio de los articulos"));
-//     PrecioTotal += precio;
-//     alert(nombre + "El total de su compra es:" + PrecioTotal);
-//   }
-// }
 while (edad < 18) {
   alert("tienes que ser mayor de 18 años para comprar");
   edad = parseInt(prompt("ingrese su edad"));
