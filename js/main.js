b2109c56ad87940ec769ef717a9e0adce333ccb2
Patricio Torres <patriciotorres1992@hotmail.com>
2023-01-26 22:48:34 -0300

    primera entrega

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const nombre = prompt("ingrese su Nombre");
+console.log(nombre);
+let edad = prompt("ingrese su edad");
+let PrecioTotal = 0;
+// if (edad < 18) {
+//   alert("tienes que ser mayor de 18 años para comprar");
+//   edad = parseInt(prompt("ingrese su edad"));
+// } else {
+//   const articulos = parseInt(prompt("ingrese numero de articulos"));
+//   for (let i = 0; (i = articulos); i++) {
+//     let precio = parseInt(prompt("ingrese precio de los articulos"));
+//     PrecioTotal += precio;
+//     alert(nombre + "El total de su compra es:" + PrecioTotal);
+//   }
+// }
+while (edad < 18) {
+  alert("tienes que ser mayor de 18 años para comprar");
+  edad = parseInt(prompt("ingrese su edad"));
+}
+if (edad >= 18) {
+  const articulos = parseInt(prompt("ingrese numero de articulos"));
+  console.log(articulos);
+  for (let i = 0; i < articulos; i++) {
+    let precio = parseInt(prompt("ingrese precio de los articulos"));
+    PrecioTotal += precio;
+  }
+  alert(nombre + " El total de su compra es: " + PrecioTotal);
+}
+console.log(edad);
